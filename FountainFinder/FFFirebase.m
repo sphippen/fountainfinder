8f97705df7ddffcfeb3d52205b846e5343530292
Spencer Phippen <spencer.phippen@gmail.com>
2015-10-01 22:14:55 -0600

    author

--- FountainFinder/FFFirebase.m
+++ FountainFinder/FFFirebase.m
@@ -2,7 +2,7 @@
 //  FFFirebase.m
 //  FountainFinder
 //
-//  Created by Andy Soto on 2015/10/01.
+//  Created by Spencer Phippen on 2015/10/01.
 //  Copyright © 2015年 sphippen. All rights reserved.
 //
 
